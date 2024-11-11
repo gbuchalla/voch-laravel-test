ef0ed6d4819e146d451ac7e913858d60991cc224
gbuchalla <guilhermebuchallaferreira@gmail.com>
2024-11-10 23:59:44 -0300

    feat: add resources routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+
+
+use App\Http\Controllers\Login;
+use App\Http\Controllers\Register;
+use App\Http\Controllers\EconomicGroupController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\HomeController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +28,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Route::get('/login', Login::class)->name('login');
+// Route::get('/register', Register::class)->name('register');  
+
+Route::post('/logout', function () {
+    Auth::logout(); 
+    return Redirect::route('login');  
+})->name('logout');
+
+Route::resources([
+    'economic-groups' => EconomicGroupController::class,
+    'brands' => BrandController::class,
+    'units' => UnitController::class,
+    'employees' => EmployeeController::class,
+]);
